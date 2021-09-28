818fb666f663f52d7985a3561793aae625ac85bf
el-zacharoo <66806423+el-zacharoo@users.noreply.github.com>
2021-09-28 16:28:19 +1300

    changes

--- model/organisation.go
+++ model/organisation.go
@@ -1,17 +1,15 @@
 package model
 
-import "github.com/biter777/countries"
-
 type Org struct {
-	ID           string            `json:"id"`
-	Name         string            `json:"name"`
-	OrgType      string            `json:"orgType"`
-	OrgSize      string            `json:"orgSize"`
-	People       []Person          `json:"people"`
-	AddressLine1 string            `json:"addressLine1"`
-	AddressLine2 string            `json:"addressLine2"`
-	Suburb       string            `json:"suburb"`
-	City         string            `json:"city"`
-	Region       string            `json:"region"`
-	Country      countries.Country `json:"country"`
+	ID           string   `json:"id"`
+	Name         string   `json:"name"`
+	OrgType      string   `json:"orgType"`
+	OrgSize      string   `json:"orgSize"`
+	People       []Person `json:"people"`
+	AddressLine1 string   `json:"addressLine1"`
+	AddressLine2 string   `json:"addressLine2"`
+	Suburb       string   `json:"suburb"`
+	City         string   `json:"city"`
+	Region       string   `json:"region"`
+	Country      string   `json:"country"`
 }
--- model/person.go
+++ model/person.go
@@ -2,25 +2,23 @@ package model
 
 import (
 	"time"
-
-	"github.com/biter777/countries"
 )
 
 type Person struct {
-	ID           string            `json:"id"`
-	FirstName    string            `json:"firstName"`
-	LastName     string            `json:"lastName"`
-	BirthDate    string            `json:"birthDate"`
-	Email        string            `json:"email"`
-	Phone        string            `json:"phone"`
-	Product      []Product         `json:"product"`
-	Date         time.Time         `json:"date"`
-	AddressLine1 string            `json:"addressLine1"`
-	AddressLine2 string            `json:"addressLine2"`
-	Suburb       string            `json:"suburb"`
-	City         string            `json:"city"`
-	Region       string            `json:"region"`
-	Country      countries.Country `json:"country"`
+	ID           string    `json:"id"`
+	FirstName    string    `json:"firstName"`
+	LastName     string    `json:"lastName"`
+	BirthDate    string    `json:"birthDate"`
+	Email        string    `json:"email"`
+	Phone        string    `json:"phone"`
+	Product      []Product `json:"product"`
+	Date         time.Time `json:"date"`
+	AddressLine1 string    `json:"addressLine1"`
+	AddressLine2 string    `json:"addressLine2"`
+	Suburb       string    `json:"suburb"`
+	City         string    `json:"city"`
+	Region       string    `json:"region"`
+	Country      string    `json:"country"`
 }
 
 type Page struct {
