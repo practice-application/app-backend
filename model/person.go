c114d08a52eddc24e11a5abf0b70a1144263e05b
el-zacharoo <66806423+el-zacharoo@users.noreply.github.com>
2021-09-28 15:48:45 +1300

    changed struct

--- model/person.go
+++ model/person.go
@@ -2,19 +2,25 @@ package model
 
 import (
 	"time"
+
+	"github.com/biter777/countries"
 )
 
 type Person struct {
-	ID        string    `json:"id"`
-	FirstName string    `json:"firstName"`
-	LastName  string    `json:"lastName"`
-	BirthDate string    `json:"birthDate"`
-	Email     string    `json:"email"`
-	Phone     string    `json:"phone"`
-	Product   []Product `json:"product"`
-	Date      time.Time `json:"date"`
-	Address   Location  `json:"address"`
-	Country   string    `json:"country"`
+	ID           string            `json:"id"`
+	FirstName    string            `json:"firstName"`
+	LastName     string            `json:"lastName"`
+	BirthDate    string            `json:"birthDate"`
+	Email        string            `json:"email"`
+	Phone        string            `json:"phone"`
+	Product      []Product         `json:"product"`
+	Date         time.Time         `json:"date"`
+	AddressLine1 string            `json:"addressLine1"`
+	AddressLine2 string            `json:"addressLine2"`
+	Suburb       string            `json:"suburb"`
+	City         string            `json:"city"`
+	Region       string            `json:"region"`
+	Country      countries.Country `json:"country"`
 }
 
 type Page struct {
