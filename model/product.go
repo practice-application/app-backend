18c424a6a6da9bae3fca3b1f9b070c31e0a7ddba
el-zacharoo <66806423+el-zacharoo@users.noreply.github.com>
2021-09-30 16:08:57 +1300

    image in struct

--- model/product.go
+++ model/product.go
@@ -1,13 +1,17 @@
 package model
 
-import "time"
+import (
+	"image"
+	"time"
+)
 
 type Product struct {
-	ID          string    `json:"id"`
-	Name        string    `json:"name"`
-	Price       string    `json:"price"`
-	Description string    `json:"description"`
-	Date        time.Time `json:"date"`
+	ID          string      `json:"id"`
+	Name        string      `json:"name"`
+	Price       string      `json:"price"`
+	Description string      `json:"description"`
+	Date        time.Time   `json:"date"`
+	Image       image.Alpha `json:"image"`
 }
 
 type ProductPage struct {
