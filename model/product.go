a0d72792d7f0e95b64b638383c90c0f07047173b
el-zacharoo <66806423+el-zacharoo@users.noreply.github.com>
2021-10-08 13:50:30 +1300

    update

--- model/product.go
+++ model/product.go
@@ -10,16 +10,9 @@ type Product struct {
 	Price       string    `json:"price"`
 	Description string    `json:"description"`
 	Date        time.Time `json:"date"`
-	Image       Image     `json:"image"`
 }
 
 type ProductPage struct {
 	Data    []Product `json:"data"`
 	Matches int64     `json:"matches"`
-}
-
-type Image struct {
-	ID      string `json:"id"`
-	DataUrl string `json:"data_url"`
-	Name    string `json:"name"`
 }
