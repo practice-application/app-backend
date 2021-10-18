5b8836286da30fc50bd05ee932776dedab686538
el-zacharoo <66806423+el-zacharoo@users.noreply.github.com>
2021-10-18 13:37:52 +1300

    test

--- model/product.go
+++ model/product.go
@@ -5,22 +5,18 @@ import (
 )
 
 type Product struct {
-	ID          string    `json:"id"`
-	Name        string    `json:"name"`
-	Price       string    `json:"price"`
-	Description string    `json:"description"`
-	Date        time.Time `json:"date"`
-	ImageID     string    `json:"imageID"`
-	User        string    `json:"user"`
-	Tags        []Tags    `json:"tags"`
-	Category    string    `json:"category"`
+	ID          string        `json:"id"`
+	Name        string        `json:"name"`
+	Price       string        `json:"price"`
+	Description string        `json:"description"`
+	Date        time.Time     `json:"date"`
+	ImageID     string        `json:"imageID"`
+	User        string        `json:"user"`
+	Tags        [0 & 5]string `json:"tags"`
+	Category    string        `json:"category"`
 }
 
 type ProductPage struct {
 	Data    []Product `json:"data"`
 	Matches int64     `json:"matches"`
-}
-
-type Tags struct {
-	Item string `json:"item"`
 }
